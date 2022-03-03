5f4320fa988f7c4790b28603e5b4096107f81d32
Dendisreya <sreyareddydendi13@gmail.com>
2022-03-04 04:08:08 +0530

    add

---
+++ 3_Implementation/main.c
@@ -0,0 +1,32 @@
+/**
+ * @file main.c
+ * @author Sreya Dendi 
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __main_c__
+#define __main_c__
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+typedef struct MainMenu{
+    int number,days;
+    double val,units,voltage,current,array[100],reactance,days1,number1;
+
+}MainMenu;
+double Resistor();
+/**
+ * @brief 
+ * 
+ */
+double Battery();
+/**
+ * @brief 
+ * 
+ */
+
+#endif
